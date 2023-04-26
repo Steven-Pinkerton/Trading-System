222b078a789d27b61768a6ba79b8ec5762bffa9f
Steven <steven.a.pinkerton@gmail.com.com>
2023-04-26 01:50:43 +0100

    ArticleExtraction.hs made

---
+++ src/ArticleExtraction/ArticleExtraction.hs
@@ -0,0 +1,6 @@
+module ArticleExtraction (
+  ) where
+
+-- ...
+
+import ArticleExtraction.Article (Article) -- Import your Article data type and related functionsimport ArticleExtraction.IO (loadArticles, saveArticles)
---
+++ src/ArticleExtraction/IO.hs
@@ -0,0 +1,27 @@
+module ArticleExtraction.IO (
+  saveArticles,
+  loadArticles,
+) where
+
+import ArticleExtraction.Article (Article, articleToText, textToArticle)
+import Data.Text (Text)
+import Data.Text.IO qualified as TIO
+import System.Directory (doesFileExist)
+import System.FilePath ((</>))
+
+-- | 'saveArticles' function takes a file path and a list of 'Article's and saves them to the specified file.
+saveArticles :: FilePath -> [Article] -> IO ()
+saveArticles filePath articles = do
+  let content = map articleToText articles
+  writeFileText filePath (unlines content)
+
+-- | 'loadArticles' function takes a file path and returns a list of 'Article's read from the specified file.
+loadArticles :: FilePath -> IO [Article]
+loadArticles filePath = do
+  fileExists <- doesFileExist filePath
+  if fileExists
+    then do
+      content <- TIO.readFile filePath
+      let articles = map textToArticle (lines content)
+      return (catMaybes articles)
+    else return []
