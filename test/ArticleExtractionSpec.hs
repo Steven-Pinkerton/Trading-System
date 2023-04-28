fca985092278ec65f636bdbad4afe9538ef4d1c9
Steven <steven.a.pinkerton@gmail.com.com>
2023-04-28 16:13:12 +0100

    Test for articleextraction added

---
+++ test/ArticleExtractionSpec.hs
@@ -0,0 +1,18 @@
+module ArticleExtractionSpec (spec) where
+
+import ArticleExtraction.ArticleExtraction (Article (..), extractAndPreprocess)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "ArticleExtraction.extractAndPreprocess" $ do
+    it "should extract and preprocess articles from a given URL" $ do
+      let testUrl = "https://example.com/test_articles.html"
+      result <- extractAndPreprocess testUrl
+      case result of
+        Left err -> expectationFailure $ "Failed to fetch articles: " ++ show err
+        Right articles -> do
+          length articles `shouldBe` 3
+          title (head articles) `shouldBe` "Sample Article 1"
+
+-- Add more assertions to check the content of the extracted articles
