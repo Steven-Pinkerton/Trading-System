5e8758647fe040e0418bc4260d485e8c299c5a71
Steven <steven.a.pinkerton@gmail.com.com>
2023-05-05 17:48:44 +0100

    expected test output folders and files made

--- Test/Scraper/ParsersSpec.hs
+++ Test/Scraper/ParsersSpec.hs
@@ -4,6 +4,7 @@ module Scraper.ParsersSpec where
 
 import Data.Text.Lazy.Encoding ()
 import Scraper.Parsers (fetchArticleContent)
+import System.IO (readFile)
 import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 
 main :: IO ()
@@ -13,11 +14,13 @@ spec :: Spec
 spec = do
   describe "extractContent" $ do
     it "extracts the correct content from the Mario article" $ do
-      marioHtml <- readFileLBS "test_data/mario.html"
-      marioContent <- fetchArticleContent (decodeUtf8 marioHtml)
-      marioContent `shouldBe` Just "This is the expected content from the Mario article."
+      marioHtml <- readFile "test_data/mario.html"
+      marioContent <- fetchArticleContent marioHtml
+      expectedContent <- readFile "expected_test_results/mario.expected.txt"
+      marioContent `shouldBe` Just expectedContent
 
     it "extracts the correct content from the Meta article" $ do
-      metaHtml <- readFileLBS "test_data/meta.html"
-      metaContent <- fetchArticleContent (decodeUtf8 metaHtml)
-      metaContent `shouldBe` Just "This is the expected content from the Meta article."
+      metaHtml <- readFile "test_data/meta.html"
+      metaContent <- fetchArticleContent metaHtml
+      expectedContent <- readFile "expected_test_results/meta.expected.txt"
+      metaContent `shouldBe` Just expectedContent
