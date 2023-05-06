acf69a7fed5134e556442e407a1e458c3f41f59b
Steven <steven.a.pinkerton@gmail.com.com>
2023-05-06 12:44:55 +0100

    Carried on work with ArticleExtraction

--- src/ArticleExtraction/ArticleExtraction.hs
+++ src/ArticleExtraction/ArticleExtraction.hs
@@ -12,7 +12,6 @@ import Data.Text (isInfixOf)
 import Network.HTTP.Client (
   HttpException (HttpExceptionRequest),
   HttpExceptionContent (StatusCodeException),
-  Manager,
   Request (host),
   Response (responseBody, responseCookieJar, responseVersion),
   host,
@@ -22,7 +21,9 @@ import Network.HTTP.Client (
 import Network.HTTP.Types (Status (..), http11)
 import Scraper.GamesIndustry (extractArticlesGamesIndustry)
 import Scraper.Parsers (extractArticles)
-import Scraper.Requests (fetchPage, fetchPageWithRetry)
+import Scraper.Requests (fetchPageWithRetry)
+import Data.ByteString qualified as ByteString
+
 
 -- | 'extractAndPreprocess' function takes a URL and returns a list of preprocessed articles.
 extractAndPreprocess :: String -> IO (Either HttpException [Article])
@@ -34,13 +35,12 @@ extractAndPreprocess url' = do
         Just httpErr -> return $ Left httpErr
         Nothing -> return $ Left (toHttpException someErr)
     Right content' -> do
-      let decodedContent = decodeUtf8 content'
-          articles = extractArticlesForSite (toText url') decodedContent
+      let articles = extractArticlesForSite (toText url') content'
           preprocessedArticles = map preprocessArticle articles
       return $ Right preprocessedArticles
 
 toHttpException :: SomeException -> HttpException
-toHttpException someErr = HttpExceptionRequest defaultRequest (StatusCodeException response ())
+toHttpException someErr = HttpExceptionRequest defaultRequest (StatusCodeException response ByteString.empty)
   where
     defaultRequest = defaultRequest {host = "example.com"}
     response :: Response ()
@@ -48,11 +48,10 @@ toHttpException someErr = HttpExceptionRequest defaultRequest (StatusCodeExcepti
       response
         { responseStatus = Status 999 "Unknown Error"
         , responseVersion = http11
-        , responseHeaders = [(mk (encodeUtf8 "X-Error-Message"), encodeUtf8 (displayException someErr))]
+        , responseHeaders = [(mk (encodeUtf8 ("X-Error-Message" :: Text)), encodeUtf8 (displayException someErr))]
         , responseBody = ()
         , responseCookieJar = mempty
         }
-
 -- | 'preprocessArticle' function takes an 'Article' and returns a preprocessed 'Article'.
 preprocessArticle :: Article -> Article
 preprocessArticle (MkArticle title' url' content') =
@@ -62,22 +61,23 @@ preprocessArticle (MkArticle title' url' content') =
 data Site = GamesIndustryBiz | OtherSite
 
 identifySite :: Text -> Site
-identifySite url =
-  if "gamesindustry.biz" `isInfixOf` url
+identifySite urlText =
+  if "gamesindustry.biz" `isInfixOf` urlText
     then GamesIndustryBiz
     else OtherSite
 
-extractArticlesForSite :: Text -> Text -> [Article]
+extractArticlesForSite :: Text -> ByteString -> [Article]
 extractArticlesForSite siteUrl html =
-  case identifySite siteUrl of
-    GamesIndustryBiz -> extractArticlesGamesIndustry html
-    OtherSite -> extractArticles html
+  let decodedHtml = decodeUtf8 html
+   in case identifySite siteUrl of
+        GamesIndustryBiz -> extractArticlesGamesIndustry decodedHtml
+        OtherSite -> extractArticles html
 
-scrapeArticles :: Text -> Manager -> IO [Article]
-scrapeArticles url manager = do
-  result <- fetchPageWithRetry url
+scrapeArticles :: Text -> IO [Article]
+scrapeArticles urlText = do
+  result <- fetchPageWithRetry urlText
   case result of
     Left _ -> return [] -- You can decide how to handle the error case here
     Right html -> do
-      let articles = extractArticlesForSite url (decodeUtf8 html)
+      let articles = extractArticlesForSite urlText html
       return articles
