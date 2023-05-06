93bd0cff498f8089882f3d6fa058a536047f02ac
Steven <steven.a.pinkerton@gmail.com.com>
2023-05-06 12:48:18 +0100

    Fixed minor issue in IO

--- src/ArticleExtraction/IO.hs
+++ src/ArticleExtraction/IO.hs
@@ -1,17 +1,20 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use readFileText" #-}
 module ArticleExtraction.IO (
   saveArticles,
   loadArticles,
 ) where
 
-import ArticleExtraction.Article (Article, articleToText, textToArticle)
+import ArticleExtraction.Article (articleToText, textToArticle)
 import Data.Text.IO qualified as TIO
 import System.Directory (doesFileExist)
+import Common (Article (..))
 
 -- | 'saveArticles' function takes a file path and a list of 'Article's and saves them to the specified file.
 saveArticles :: FilePath -> [Article] -> IO ()
 saveArticles filePath articles = do
-  let content = map articleToText articles
-  writeFileText filePath (unlines content)
+  let content' = map articleToText articles
+  writeFileText filePath (unlines content')
 
 -- | 'loadArticles' function takes a file path and returns a list of 'Article's read from the specified file.
 loadArticles :: FilePath -> IO [Article]
@@ -19,7 +22,7 @@ loadArticles filePath = do
   fileExists <- doesFileExist filePath
   if fileExists
     then do
-      content <- TIO.readFile filePath
-      let articles = map textToArticle (lines content)
+      content' <- TIO.readFile filePath
+      let articles = map textToArticle (lines content')
       return (catMaybes articles)
     else return []
