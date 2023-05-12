c29e37243ab4ec4082a7e23ec93a9d9dbc7814cf
Steven <steven.a.pinkerton@gmail.com.com>
2023-05-12 18:03:24 +0100

    Added polygon test

---
+++ Test/ScraperSpec/Polygon.hs
@@ -0,0 +1,41 @@
+module ScraperSpec.Polygon where
+
+
+import Data.Aeson (decode)
+import Data.ByteString.Lazy.Char8 as C8 ()
+import Data.Text.Lazy.Encoding qualified as LE
+import Test.Hspec (Spec, describe, it, shouldBe)
+import Text.HTML.TagSoup (parseTags)
+
+import Common (Article, convertTag)
+import Data.Maybe (fromJust)
+import Scraper.Polygon (extractArticlesPolygon, parsePolygonArticle)
+
+-- | Function to perform the test.
+spec :: Spec
+spec = do
+  describe "extractArticlesPolygon" $ do
+    it "extracts the correct articles from Polygon's index page" $ do
+      -- Load the sample index page HTML.
+      htmlBytes <- readFileLBS "test/html/polygon_index.html"
+      let html = LE.decodeUtf8 htmlBytes
+      -- Extract the articles from the HTML.
+      let articles = extractArticlesPolygon (toText html) -- Convert to strict Text
+      -- Load the expected articles.
+      expectedArticlesBytes <- readFileLBS "test/expected/polygon_index_articles.txt"
+      let expectedArticles = decode expectedArticlesBytes :: Maybe [Article] -- specify the type as Maybe [Article]
+      -- Check if the extracted articles match the expected articles.
+      articles `shouldBe` fromJust expectedArticles -- use fromJust to extract the value from Maybe
+  describe "parsePolygonArticle" $ do
+    it "parses the correct article from Polygon's individual article page" $ do
+      -- Load the sample article page HTML.
+      htmlBytes <- readFileLBS "test/html/polygon_article.html"
+      let html = LE.decodeUtf8 htmlBytes
+      -- Parse the article from the HTML.
+      let tags = map convertTag $ parseTags $ toString html
+      let article = parsePolygonArticle "https://www.polygon.com/sample-article" tags
+      -- Load the expected article.
+      expectedArticleBytes <- readFileLBS "test/expected/polygon_article.txt"
+      let expectedArticle = decode expectedArticleBytes :: Maybe Article -- specify the type as Maybe Article
+      -- Check if the parsed article matches the expected article.
+      article `shouldBe` expectedArticle -- Ensure the types match
--- src/Common.hs
+++ src/Common.hs
@@ -2,10 +2,10 @@
 
 module Common (
   Article (..),
-) where
+convertTag) where
 
 import Data.Aeson (FromJSON, ToJSON)
-import Data.Text ()
+import Text.HTML.TagSoup ( Tag(..) )
 
 data Article = Article
   { title :: Text
@@ -17,3 +17,11 @@ data Article = Article
 -- We derive these instances so that we can convert Article to/from JSON
 instance ToJSON Article
 instance FromJSON Article
+
+convertTag :: Tag String -> Tag Text
+convertTag (TagOpen str attrs) = TagOpen (toText str) (map (bimap toText toText) attrs)
+convertTag (TagClose str) = TagClose (toText str)
+convertTag (TagText str) = TagText (toText str)
+convertTag (TagComment str) = TagComment (toText str)
+convertTag (TagWarning str) = TagWarning (toText str)
+convertTag (TagPosition row col) = TagPosition row col
