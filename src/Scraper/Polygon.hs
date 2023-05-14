66c3d5d6e72aa2b53f427a931d77a5a82a17c61b
Steven <steven.a.pinkerton@gmail.com.com>
2023-05-14 03:08:14 +0100

    Fixed some stuff for polygon code

--- src/Scraper/Polygon.hs
+++ src/Scraper/Polygon.hs
@@ -22,10 +22,13 @@ extractArticlesPolygon html = do
   let doc = case Text.XML.parseLBS def (encodeUtf8 html) of
         Left _ -> error "Failed to parse HTML"
         Right d -> d
+
   -- Create a cursor from the XML document.
   let cursor = fromDocument doc
+
   -- Find all article nodes by their class attribute.
   let articleNodes = cursor $// Cursor.element "div" >=> Cursor.attributeIs "class" "c-entry-box--compact c-entry-box--compact--article"
+
   -- Extract articles from the nodes.
   mapMaybe extractArticleFromNode articleNodes
   where
@@ -35,7 +38,7 @@ extractArticlesPolygon html = do
       let urlText = listToMaybe urlNodes
 
       -- Find the title nodes and get their content.
-      let titleNodes = articleNode $// Cursor.element "title" &/ Cursor.content
+      let titleNodes = articleNode $// Cursor.element "h2" >=> Cursor.attributeIs "class" "c-entry-box--compact__title" &/ Cursor.content
       let titleText = listToMaybe titleNodes
 
       -- Find the content nodes and combine their content.
