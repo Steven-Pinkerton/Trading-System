2d0347ed67624f8b5844295429e4e15268a96dd2
Steven <steven.a.pinkerton@gmail.com.com>
2023-06-20 03:53:39 +0100

    More changes

--- src/Scraper/VentureBeat.hs
+++ src/Scraper/VentureBeat.hs
@@ -1,18 +1,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
 {-# HLINT ignore "Use newtype instead of data" #-}
 
 module Scraper.VentureBeat (
   URL (..),
+  extractArticlesVB,
+  parseVBArticle,
+  fetchVBArticleContent,
 ) where
 
 import Common (Article (..))
-import Text.XML (def)
-import Text.XML qualified as XML
-import Text.XML.Cursor (fromDocument, ($//), (&/), Cursor, element, (&//))
-import Text.XML.Cursor qualified as Cursor
+import Control.Exception (try)
+import Data.ByteString.Lazy qualified as BL
+import Network.HTTP.Conduit (simpleHttp)
 import Scraper.RPS (URL (URL))
-
+import Text.XML (def, parseLBS)
+import Text.XML qualified as XML
+import Text.XML.Cursor (Cursor, element, fromDocument, ($//), (&/), (&//))
+import Text.XML.Cursor qualified as Cursor
 
 -- Helper function to extract URL Text
 getUrl :: URL -> Text
@@ -21,7 +27,7 @@ getUrl (URL url') = url'
 -- | Extracts articles from VentureBeat's index page HTML text.
 extractArticlesVB :: Text -> IO [Article]
 extractArticlesVB html = do
-  let docEither = XML.parseLBS def (encodeUtf8 html)
+  let docEither = XML.parseLBS def (Prelude.encodeUtf8 html)
   case docEither of
     Left _ -> return []
     Right doc -> do
@@ -53,3 +59,14 @@ parseVBArticle (URL url') cursor = do
   case titleText of
     Just title' -> Just $ Article title' url' contentText
     _ -> Nothing
+
+
+-- Fetch the HTML content of an article given its URL
+fetchVBArticleContent :: URL -> IO (Either Text Cursor)
+fetchVBArticleContent (URL url') = do
+  response <- try $ simpleHttp (toString url') :: IO (Either SomeException BL.ByteString)
+  return $ case response of
+    Left _ -> Left "Error fetching the URL"
+    Right html -> case parseLBS def html of
+      Left _ -> Left "Error parsing HTML"
+      Right doc -> Right $ fromDocument doc
